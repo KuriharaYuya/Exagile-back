bbb00415dbd846a23f0b421d4431cc96dd2bc6fc
kurihara.sk <nikoand01@gmail.com>
2023-01-02 23:08:54 +0900

    update: revise signup flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, only: [:show]
+      post "/signup", to: "users#create"
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_01_052306) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_132819) do
   create_table "users", primary_key: "uid", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
