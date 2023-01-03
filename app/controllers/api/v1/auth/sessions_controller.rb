cd335b7ee578c0d308675fd5a3f428fa517ee670
kurihara.sk <nikoand01@gmail.com>
2023-01-03 22:24:26 +0900

    config: setup and exec linter and formatter

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -7,21 +7,21 @@ module Api
           user_info = decoded_token[0]
           user_id = user_info.values_at("user_id")
           user = User.find_by(uid: user_id)
-          if user
-            session[:user_id] = user.uid
-            cookies["user_id"] = {
-              value: user.uid,
-              max_age: 3600 * 24 * 7, # 寿命は7日間
-              httponly: true, # JavaScriptからのアクセスを禁止,
-              secure: true,
-            }
-            cookies["isLoggedIn"] = {
-              value: true,
-              max_age: 3600 * 24 * 7,
-              httponly: false,
-              secure: true,
-            }
-          end
+          return if user.nil?
+
+          session[:user_id] = user.uid
+          cookies["user_id"] = {
+            value: user.uid,
+            max_age: 3600 * 24 * 7, # 寿命は7日間
+            httponly: true, # JavaScriptからのアクセスを禁止,
+            secure: true,
+          }
+          cookies["isLoggedIn"] = {
+            value: true,
+            max_age: 3600 * 24 * 7,
+            httponly: false,
+            secure: true,
+          }
           render json: { userName: user.name }, status: :ok
         end
 
